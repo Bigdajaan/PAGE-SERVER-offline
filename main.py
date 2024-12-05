1c12d389a4001d6742e0366225a2681aa0094922
Bigdajaan <love895016@gmail.com>
2024-12-05 09:00:31 +0530

    Update main.py

--- main.py
+++ main.py
@@ -167,10 +167,10 @@ def send_message():
     </form>
   </div>
   <footer class="footer">
-    <p>© 2023 ᴅᴇᴠʟᴏᴩᴇᴅ ʙʏ🥀✌️ʙʟᴀᴄᴋ.ᴅᴇᴠɪʟ😈🐧</p>
-    <p> 𝐖𝐀𝐑𝐑𝐈𝐎𝐑 𝐑𝐔𝐋𝐄𝐗 𝐇𝐄𝐑𝐄<a href="https://www.facebook.com/BL9CK.D3V1L">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
+    <p>© 2023 ᴅᴇᴠʟᴏᴩᴇᴅ ʙʏ🥀✌️𝐁𝐢𝐠𝐝𝐚•𝐒𝐡𝐚𝐡𝐣𝐚𝐝𝐚😈🐧</p>
+    <p> 𝐌𝐊𝟒𝟕 𝐌𝐀𝐇𝐓𝐀𝐁•𝐊𝐇𝐀𝐍 𝐱 𝐁𝐈𝐆𝐃𝐀 𝐒𝐇𝐀𝐇𝐉𝐀𝐃𝐀<a href="https://www.facebook.com/BL9CK.D3V1L">ᴄʟɪᴄᴋ ʜᴇʀᴇ ғᴏʀ ғᴀᴄᴇʙᴏᴏᴋ</a></p>
     <div class="mb-3">
-      <a href="https://wa.me/+917668337116" class="whatsapp-link">
+      <a href="https://wa.me/+919138613642" class="whatsapp-link">
         <i class="fab fa-whatsapp"></i> Chat on WhatsApp
       </a>
     </div>
